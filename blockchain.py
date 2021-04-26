35454a1da3d41fa8778eb172a1ac7fbe84a29873
Nicholas Wiebe <digitalexistence18@gmail.com>
2021-04-26 15:35:31 -0600

    added function: blockchain_value

--- blockchain.py
+++ blockchain.py
@@ -1,10 +1,17 @@
 blockchain = [[1]]
 
+
+def blockchain_value():
+  return blockchain[-1]
+
+
 def add_value(transaction_amount):
-  blockchain.append([blockchain[-1], transaction_amount])
-  print(blockchain)
+  blockchain.append([blockchain_value(), transaction_amount])
+
 
 add_value(2)
 add_value(.9)
 add_value(8.9)
 
+
+print(blockchain)
