02a82cc697cf8dd677c0ef3360084621458ab501
Nicholas Wiebe <digitalexistence18@gmail.com>
2021-04-27 16:11:43 -0600

    refactor variables, to use functions

--- ex01_variables.py
+++ ex01_variables.py
@@ -1,10 +1,15 @@
+#this needs to run with python3, otherwise 
+#In Python 2, getting input as plain text is done via raw_input instead of input. 
+#The Python 2 version of input does eval(raw_input(prompt)), 
 
 def name():
-  return input('what is your name? ')
+  return input('What is your name? ')
+
 
 def age():
-  return int(input('what is your age? '))
+  return int(input('What is your age? '))
 
-some_name = input('what is your name? ')
+name = name()
+age = age()
 
-print(some_name)
+print(name, age)
