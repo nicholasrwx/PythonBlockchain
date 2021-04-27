a00f1ef1bdd1aa1638e5afbb0e4bb96860ca60d4
Nicholas Wiebe <digitalexistence18@gmail.com>
2021-04-27 14:59:26 -0600

    refactor with feat: get_user_input function

--- blockchain.py
+++ blockchain.py
@@ -8,14 +8,18 @@ def blockchain_value():
 def add_value(transaction_amount, last_transaction=[1]):
   blockchain.append([last_transaction, transaction_amount])
 
+def get_user_input():
+  return float(input('Your transaction amount please > '))
+
+
 #I can wrap input with float, and the string won't cause an issue, while it handles the input
-tx_amount = float(input('Your transaction amount please > '))
+tx_amount = get_user_input()
 add_value(tx_amount)
 
-tx_amount = float(input('Your transaction amount please > '))
+tx_amount = get_user_input()
 add_value(last_transaction=blockchain_value(), transaction_amount=tx_amount)
 
-tx_amount = float(input('Your transaction amount please > '))
+tx_amount = get_user_input()
 add_value(tx_amount, blockchain_value())
 
 
