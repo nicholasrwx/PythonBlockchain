8b017320057a32634758914e4190882bc1da0d87
Nicholas Wiebe <digitalexistence18@gmail.com>
2021-04-30 14:06:38 -0600

    using a for-in loop to output blockchain values

--- blockchain.py
+++ blockchain.py
@@ -1,26 +1,38 @@
+# Initialize empty blockchain list
 blockchain = []
 
 
+# return the last value in the blockchain
 def blockchain_value():
-  return blockchain[-1]
+    return blockchain[-1]
 
 
+# append the previous value and new value to the blockchain
 def add_value(transaction_amount, last_transaction=[1]):
-  blockchain.append([last_transaction, transaction_amount])
+    blockchain.append([last_transaction, transaction_amount])
 
+
+# User input function
 def get_user_input():
-  return float(input('Your transaction amount please > '))
+    return float(input('Your transaction amount please > '))
 
 
-#I can wrap input with float, and the string won't cause an issue, while it handles the input
+# Get user input, add it to the blockchain.
 tx_amount = get_user_input()
 add_value(tx_amount)
 
+# Get 2nd user input, add it to the blockchain.
 tx_amount = get_user_input()
 add_value(last_transaction=blockchain_value(), transaction_amount=tx_amount)
 
+# Get 3rd user input, add it to the blockchain.
 tx_amount = get_user_input()
 add_value(tx_amount, blockchain_value())
 
 
-print(blockchain)
+# Output the blockchain list to the console
+for block in blockchain:
+    print('Output blockchain')
+    print(block)
+
+print('Done')
