f2a4ebfeea3dc13e496166c578afa52af09f4e47
Nicholas Wiebe <digitalexistence18@gmail.com>
2021-05-08 20:35:46 -0600

    exercise 2: while loop that emptys list

--- ex02_loops_and_conditions.py
+++ ex02_loops_and_conditions.py
@@ -1,15 +1,26 @@
-#list of names
-names = ['John', 'James', 'Jason', 'Jennifer', 'Joy', 'Joe', 'JuNior', 'Jimmy', 'Jim', 'Jay']
+# list of names
+names = ['John', 'James', 'Jason', 'Jennifer',
+         'Joy', 'Joe', 'JuNior', 'Jimmy', 'Jim', 'Jay']
 
-#loop through names list
+
+# loop through the names list
 for name in names:
-  #print name length
-  print(str(len(name)))
+    # print name length
+    print(str(len(name)))
 
-  #print names with a length greater than 5 that contain 'n' or 'N'
-  if len(name) > 5:
-    for index in range(len(name)):
-      if name[index] == 'n' or name[index] == 'N':
-        print(name)
-        break
+    # print names with a length greater than 5 that contain 'n' or 'N'
+    if len(name) > 5:
+        for index in range(len(name)):
+            if name[index] == 'n' or name[index] == 'N':
+                print(name)
+                break
 
+
+# clear all names from list
+while names:
+  if len(names) == 1:
+    names.pop()
+    break
+  names.pop()
+
+print('\nthe names list has been cleared:\n\n\t names =', names, '\n')
