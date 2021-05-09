e8b6155294b37ecab0fa838e5007fd5c3dd16cde
Nicholas Wiebe <digitalexistence18@gmail.com>
2021-05-08 20:08:58 -0600

    exercise 2: if conditions

--- ex02_loops_and_conditions.py
+++ ex02_loops_and_conditions.py
@@ -1,7 +1,15 @@
 #list of names
-names = ['John', 'James', 'Jason', 'Jennifer', 'Joy', 'Joe', 'Junior', 'Jimmy', 'Jim', 'Jay']
+names = ['John', 'James', 'Jason', 'Jennifer', 'Joy', 'Joe', 'JuNior', 'Jimmy', 'Jim', 'Jay']
 
+#loop through names list
 for name in names:
-  print('name: ' + name + ', name-length: ' + str(len(name)))
+  #print name length
+  print(str(len(name)))
 
+  #print names with a length greater than 5 that contain 'n' or 'N'
+  if len(name) > 5:
+    for index in range(len(name)):
+      if name[index] == 'n' or name[index] == 'N':
+        print(name)
+        break
 
