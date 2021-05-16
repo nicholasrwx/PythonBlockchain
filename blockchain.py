5c73edaa3e8a53bcfa3d47fc653ca81e0932285f
Nicholas Wiebe <digitalexistence18@gmail.com>
2021-05-16 17:53:12 -0600

    Update: valid_proof() to use hexdigest()

--- blockchain.py
+++ blockchain.py
@@ -12,7 +12,7 @@ participants = {'Max'}
 
 def valid_proof(transactions, last_hash, proof):
     guess = (str(transactions) + str(last_hash) + str(proof)).encode()
-    guess_hash = hl.sha256(guess)
+    guess_hash = hl.sha256(guess).hexdigest()
     print(guess_hash, "GUESS HASH")
     return guess_hash[0:2] == '00'
 
