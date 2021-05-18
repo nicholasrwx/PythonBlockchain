ea776324b5591aff84dbed5c88dfaf4016f2f178
Nicholas Wiebe <digitalexistence18@gmail.com>
2021-05-18 16:19:29 -0600

    Update: use close() to ensure write happens

--- files.py
+++ files.py
@@ -1,4 +1,8 @@
 f = open('demo.txt', mode='w')
 f.write('Hello from Python!') 
+# you need to call close, 
+# otherwise it wont write to the file 
+# until the program has ended
+f.close()
 
 user_input = input('Please enter input: ')
