07efaebb3732bac39001ecb6479bce3fad28204f
Nicholas Wiebe <digitalexistence18@gmail.com>
2021-05-18 16:31:39 -0600

    Update: use r and a mode with open()

--- files.py
+++ files.py
@@ -1,8 +1,24 @@
-f = open('demo.txt', mode='w')
-f.write('Hello from Python!') 
-# you need to call close, 
-# otherwise it wont write to the file 
-# until the program has ended
-f.close()
+# f = open('demo.txt', mode='w')
+# f.write('Hello from Python!') 
+# # you need to call close, 
+# # otherwise it wont write to the file 
+# # until the program has ended
+# f.close()
+# user_input = input('Please enter input: ')
 
-user_input = input('Please enter input: ')
+
+
+# #read only (if demo.txt doesn't exist, nothing will be displayed)
+# f = open('demo.txt', mode='r')
+# file_content = f.read()
+# print(file_content)
+# f.close()
+
+
+
+# #open an existing file, and append something to it
+# f = open('demo.txt', mode='a')
+# f.write('Something\n')
+# f.close()
+
+
