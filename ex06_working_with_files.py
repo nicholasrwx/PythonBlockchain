9b60d0ebaffbdcb9fb1cef97bc3e0c8321284087
Nicholas Wiebe <digitalexistence18@gmail.com>
2021-05-20 01:50:58 -0600

    Exercise 6: working with files

---
+++ ex06_working_with_files.py
@@ -0,0 +1,46 @@
+#1) Write a short Python script which queries the user for input 
+# (infinite loop with exit possibility) and writes the input to a file.
+
+
+print('enter a response, or q to quit!')
+response = input('What is your secret? ')
+  
+with open('response.txt', mode='w') as f:
+  f.
+
+
+#open a file or 
+#create a file
+#write to it 
+#save when quit
+
+
+
+
+#2) Add another option to your user interface: 
+# The user should be able to output the data 
+# stored in the file in the terminal.
+
+
+
+
+
+
+
+#3) Store user input in a list 
+# (instead of directly adding it to the file) 
+# and write that list to the file – both with pickle and json.
+
+
+
+
+
+
+
+#4) Adjust the logic to load the 
+# file content to work with pickled/ json data.
+
+
+
+
+
