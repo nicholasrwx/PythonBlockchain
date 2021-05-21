874ca4bbd37f16987a1d1be46908058579177606
Nicholas Wiebe <digitalexistence18@gmail.com>
2021-05-21 13:52:45 -0600

    Add: add_warning() and get_warnings() setter && getter

--- oop/cars.py
+++ oop/cars.py
@@ -8,11 +8,19 @@ class Car:
 
     def __init__(self, starting_top_speed=100):
         self.top_speed = starting_top_speed
-        self.warnings = []
+        self.__warnings = []
 
     def __repr__(self):
         print('Printing...')
         return 'Top Speed: {}, Warnings: {}'.format(self.top_speed, len(self.warnings))
+
+    def add_warning(self, warning_text):
+      if len(warning_text) > 0:
+        self.__warnings.append(warning_text)
+
+    def get_warnings(self):
+      return self.__warnings
+
 
     # self is not a reserved keyword
     # you could use a different name
@@ -28,15 +36,16 @@ car1 = Car()
 car1.drive()
 
 #Car.top_speed = 200
-car1.warnings.append('New Warning')
+car1.add_warning('New warning')
+car1.__warnings.append([])
 # displays instance methods in a dict format
 # without it, you would just get a memory location
 print(car1)
 
 car2 = Car(200)
 car2.drive()
-print(car2.warnings)
+print(car2.get_warnings)
 
 car3 = Car(250)
 car3.drive()
-print(car2.warnings)
+print(car2.get_warnings)
