9ffefcd5203aff220df812534d2fb765aa32b343
Nicholas Wiebe <digitalexistence18@gmail.com>
2021-05-20 23:52:15 -0600

    Update: create Instances, modify reference types

--- oop/cars.py
+++ oop/cars.py
@@ -1,12 +1,13 @@
 class Car:
   top_speed = 100
+  warnings = []
 
   #self is not a reserved keyword
   #you could use a different name
   #but the convention is to go
   #with SELF
 
-  
+
   def drive(self):
     print('I am driving but certainly not faster than {}'.format(self.top_speed))
 
@@ -14,3 +15,15 @@ class Car:
 car1 = Car()
 #call a class method
 car1.drive()
+
+#Car.top_speed = 200
+Car.warnings.append('New Warning')
+
+car2 = Car()
+car2.drive()
+print(car2.warnings)
+
+car3 = Car()
+car3.drive()
+print(car2.warnings)
+
