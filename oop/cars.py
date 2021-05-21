538c01cac8d0187c2357d94f2351e9ff24ce818e
Nicholas Wiebe <digitalexistence18@gmail.com>
2021-05-21 01:03:46 -0600

    Add: __repr__()

--- oop/cars.py
+++ oop/cars.py
@@ -1,32 +1,37 @@
 class Car:
-  top_speed = 100
-  warnings = []
+    top_speed = 100
+    warnings = []
 
-  #dunder functions = double under functions
-  #auto executes when we call the class as a function, like when we create a new instance
-  #we define instance attributes with __init__
+    # dunder functions = double under functions
+    # auto executes when we call the class as a function, like when we create a new instance
+    # we define instance attributes with __init__
 
-  def __init__(self, starting_top_speed=100):
-    self.top_speed = starting_top_speed
-    self.warnings = []
+    def __init__(self, starting_top_speed=100):
+        self.top_speed = starting_top_speed
+        self.warnings = []
+
+    def __repr__(self):
+        print('Printing...')
+        return 'Top Speed: {}, Warnings: {}'.format(self.top_speed, len(self.warnings))
+
+    # self is not a reserved keyword
+    # you could use a different name
+    # but the convention is to go
+    # with SELF
+    def drive(self):
+        print('I am driving but certainly not faster than {}'.format(self.top_speed))
 
 
-
-  #self is not a reserved keyword
-  #you could use a different name
-  #but the convention is to go
-  #with SELF
-  def drive(self):
-    print('I am driving but certainly not faster than {}'.format(self.top_speed))
-
-#create a new instance
+# create a new instance
 car1 = Car()
-#call a class method
+# call a class method
 car1.drive()
 
 #Car.top_speed = 200
 car1.warnings.append('New Warning')
-print(car1.warnings)
+# displays instance methods in a dict format
+# without it, you would just get a memory location
+print(car1)
 
 car2 = Car(200)
 car2.drive()
@@ -35,4 +40,3 @@ print(car2.warnings)
 car3 = Car(250)
 car3.drive()
 print(car2.warnings)
-
