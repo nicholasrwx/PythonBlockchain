f25064b6241d7b1df1c3798608bb5f32d18b0834
Nicholas Wiebe <digitalexistence18@gmail.com>
2021-05-21 00:00:10 -0600

    Add: __init__()

--- oop/cars.py
+++ oop/cars.py
@@ -2,12 +2,18 @@ class Car:
   top_speed = 100
   warnings = []
 
+  #dunder functions = double under functions
+  #auto executes when we call the class as a function, like when we create a new instance
+  #we define instance attributes with __init__
+
+  def __init__(self):
+    
+
+
   #self is not a reserved keyword
   #you could use a different name
   #but the convention is to go
   #with SELF
-
-
   def drive(self):
     print('I am driving but certainly not faster than {}'.format(self.top_speed))
 
