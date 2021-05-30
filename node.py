155e1a7abc17a066be31804ffa90b7f03937a8e9
Nicholas Wiebe <digitalexistence18@gmail.com>
2021-05-30 00:51:45 -0600

    Update: /broadcast-block route if condition

--- node.py
+++ node.py
@@ -138,7 +138,7 @@ def broadcast_block():
     block = values['block']
     # check to see if the index of the incoming block is 1 higher than the last block on the local blockchain
     if block['index'] == blockchain.chain[-1].index + 1:
-        pass
+        blockchain.add_block(block)
     # if incoming block index is greater than local last block index
     elif block['index'] > blockchain.chain[-1].index:
         pass
