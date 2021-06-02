04a960ef3603ac4f11ee6a79a90eba9f4339d111
Nicholas Wiebe <digitalexistence18@gmail.com>
2021-06-01 19:40:50 -0600

    Update: Format Code

--- utility/hash_util.py
+++ utility/hash_util.py
@@ -31,8 +31,10 @@ def hash_block(block):
     hashable_block = block.__dict__.copy()
 
     # we are able to access to_ordered_dict() through inheritance or through the block that is being passed?
-    hashable_block['transactions'] = [tx.to_ordered_dict()
-                                      for tx in hashable_block['transactions']]
+    hashable_block['transactions'] = [
+        tx.to_ordered_dict()
+        for tx in hashable_block['transactions']
+    ]
     print(hashable_block)
     return hash_string_256(json.dumps(hashable_block, sort_keys=True).encode())
 
