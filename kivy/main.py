570a56ade10e3768217ff6b439f98f1d23fd90b4
Nicholas Wiebe <digitalexistence18@gmail.com>
2022-02-03 20:43:19 -0700

    setup kivy code to create a white rectangle in a button

--- kivy/main.py
+++ kivy/main.py
@@ -1,10 +1,22 @@
 from kivy.app import App
 from kivy.uix.button import Button
+from kivy.lang import Builder
+
+
+Builder.load_file('kivy.kv')
+
+
+class FunkyButton(Button):
+    pass
 
 
 class KivyApp(App):
     def build(self):
-        return Button(text="Hello World")
+        return FunkyButton(
+            pos=(100, 100),
+            size_hint=(None, None),
+            size=(500, 500)
+        )
 
 
 if __name__ == '__main__':
